31484468340df5c5cb3f8701d4f8a96f84aa9d6d
Naufal N. Kusumo <nopalji375@gmail.com>
2025-04-08 23:55:36 +0700

    move

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,5 +3,20 @@ using UnityEngine.InputSystem;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
    [SerializeField] InputAction thrust;
-    
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+    }
+
+    void Update()
+    {
+        if (thrust.IsPressed() == true)
+        {
+            Debug.Log("Hello");
+        }
+    }
+
+
+
 }
