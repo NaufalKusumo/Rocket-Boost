23a6c3b2e515d381239afb98ac3c08b3d16f7f73
Naufal N. Kusumo <nopalji375@gmail.com>
2025-04-10 14:44:52 +0700

    movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
    [SerializeField] InputAction thrust; 
    // We have to add bindings to the script (bindings = space)
+    Rigidbody rb;
 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void OnEnable()
     {
         thrust.Enable();
